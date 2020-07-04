94b4958b3a3fb3ca2371e480cd55737bedc5e7e8
David Marom <davidmarom@hotmail.com>
2020-07-04 15:41:54 +0300

    modified:   server.js

--- server.js
+++ server.js
@@ -24,40 +24,71 @@ const artSchema = {
 const Article = mongoose.model("Article", artSchema);
 
 //TODO
+app.route('/articles')
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
 
-app.get("/articles", function (req, res) {
-    Article.find(function (err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-    });
-});
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Success");
+            } else {
+                res.send(err);
+            }
+        });
+    })
 
-app.post("/articles", function (req, res) {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Success");
+            } else {
+                res.send(err);
+            }
+        });
     });
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send("Success");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
-app.delete("/articles", function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send("Success");
-        } else {
-            res.send(err);
-        }
+app.route("/articles/:articleTitle")
+    .get(function (req, res) {
+        Article.findOne({
+            title: req.params.articleTitle
+        }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No Articles");
+            }
+        });
+    })
+
+    .put(function (req, res) {
+        Article.update({
+                title: req.params.articleTitle
+            }, {
+                title: req.body.title,
+                content: req.body.content
+            }, {
+                overwrite: true
+            },
+            function (err) {
+                if (!err) {
+                    res.send("success");
+                }
+            }
+        );
     });
-});
+
 
 
 app.listen(3000, function () {
